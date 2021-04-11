d612c5e9853f40fefe624a4495ac60ca8b3821ab
ericz3 <eric.fu.zhang@gmail.com>
2021-04-11 05:02:14 -0500

    removed keys

--- DeepBird/App.js
+++ DeepBird/App.js
@@ -8,7 +8,7 @@ import { Audio } from 'expo-av';
 export default function App() {
   const [recording, setRecording] = React.useState();
   const [prediction, setPrediction] = React.useState("");
-  const options = require('~/awsauth/options.json');
+  const options = require('./auth/options.json');
 
   function generateUUID() {
     var d = new Date().getTime();
